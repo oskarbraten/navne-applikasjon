c3f6cd2d65905c6967bfc241709552ff42004739
oskarbraten <oskar.braten@outlook.com>
2018-01-22 15:01:37 +0100

    Created new activity BrowseNames and added clicklistener.

--- app/src/main/java/no/hvl/dat153/navne_applikasjon/MainActivity.java
+++ app/src/main/java/no/hvl/dat153/navne_applikasjon/MainActivity.java
@@ -1,7 +1,10 @@
 package no.hvl.dat153.navne_applikasjon;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button browseNamesButton = findViewById(R.id.main_browse_names_btn);
+
+        browseNamesButton.setOnClickListener((View) -> {
+            startActivity(new Intent(MainActivity.this, BrowseNamesActivity.class));
+        });
     }
 }
